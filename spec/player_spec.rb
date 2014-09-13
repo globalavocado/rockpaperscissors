f3a9c538d692d5845c7314a6c15d97ef864b03f7
globalavocado <globalavocado@gmail.com>
2014-09-13 22:39:52 +0100

    corrected more spelling, added css file structure (not activated)

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,14 +1,14 @@
 require 'player'
 
 describe Player do 
-	let(:player) {Player.new("Stephen")}
+	let(:player) {Player.new("Kevin")}
 	it "has a name" do
-		expect(player.name).to eq"Stephen"
+		expect(player.name).to eq"Kevin"
 	end
 
 	it "can make a pick" do 
-		player.picks = "Rock"
-		expect(player.pick).to eq("Rock")
+		player.picks = "rock"
+		expect(player.pick).to eq("rock")
 	end
 
 
